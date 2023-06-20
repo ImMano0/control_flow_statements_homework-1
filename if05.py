6fd6a4d19bcd5fce52750a45731d6d3fdd611fc0
ImMano0 <UrMa1371@gmail.com>
2023-06-20 23:04:24 +0500

    05-misol

--- if05.py
+++ if05.py
@@ -8,4 +8,13 @@ def main(a,b,c):
     returns:
         integer: the number of negative numbers in the given numbers
     """
-    return
+    n=0
+    if a<0:
+        n=n+ 1
+    if b<0:
+        n=n+ 1
+    if c<0:
+        n=n+ 1
+    return n 
+
+print(main(-13,-17,-14))
