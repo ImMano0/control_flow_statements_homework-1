22f8dcfbb984aca8b35617617c7722e5aec60c0b
ImMano0 <UrMa1371@gmail.com>
2023-06-20 23:04:40 +0500

    07-misol

--- if07.py
+++ if07.py
@@ -12,4 +12,16 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a>0 and a%2 == 1: 
+        "positive odd number"
+    if a>0 and a%2 ==  1:
+        "positive ever number"
+    if a>0 and a%2 ==  0:
+        "negative odd number"
+    if a>0 and a%2 ==  0:
+        "negative ever number"
+    if a==0:
+        "the number is zero" 
+    return "the number is zero"
+
+print(main(2))
